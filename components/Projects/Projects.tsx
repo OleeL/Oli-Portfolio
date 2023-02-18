5c39fda70cf4787da9cc082549024ba7a46d07d6
Oli Legg <oliglegg@hotmail.co.uk>
2023-02-18 18:48:35 +0000

    change: opens project links in a new tab

--- components/Projects/Projects.tsx
+++ components/Projects/Projects.tsx
@@ -25,7 +25,7 @@ const Project: FC<Project> = ({ url, name, description, image }) => {
         <div
             className="home-box project-box"
             onClick={() => {
-                BulokeWindow.redirect(url);
+                BulokeWindow.nextOpen(url);
             }}
             onMouseEnter={() => setHovered(true)}
             onMouseLeave={() => setHovered(false)}>
