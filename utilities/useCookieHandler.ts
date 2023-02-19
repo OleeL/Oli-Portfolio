e5b72b1106bb0881ac402f865f07c6634aafcd8c
Oli Legg <oliglegg@hotmail.co.uk>
2023-02-19 10:42:14 +0000

    add: cookie handler (could need it in the future)

---
+++ utilities/useCookieHandler.ts
@@ -0,0 +1,12 @@
+export const useCookieHandler = () => {
+    const addCookie = (key: string, value: string, durationDays: number) => {
+        const exdate = new Date();
+        exdate.setDate(exdate.getDate() + durationDays);
+        const cValue =
+            encodeURI(value) +
+            (durationDays == null ? '' : `; expires=${exdate.toUTCString()}`);
+        document.cookie = `${key}=${cValue}`;
+    };
+
+    return { addCookie };
+};
