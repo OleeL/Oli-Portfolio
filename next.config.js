d642aa2bf2ce02bb7224987c9fd13ee08a43b67c
Oli Legg <oliglegg@hotmail.co.uk>
2024-07-24 00:29:26 +0100

    csp implemented

--- next.config.js
+++ next.config.js
@@ -28,6 +28,27 @@ const nextConfig = {
           }
         ],
       },
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self' data: https:; connect-src 'self' https:; font-src 'self' https: data:; object-src 'none'; upgrade-insecure-requests",
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY',
+          },
+          {
+            key: 'X-XSS-Protection',
+            value: '1; mode=block',
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+        ],
+      },
     ]
   }
 }
